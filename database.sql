d6cd2c8c15566e95b3d90a1b85b4b90d5d2919ba
Hub E Ali <58108501+Ali15700@users.noreply.github.com>
2022-07-13 06:17:10 +0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -6,12 +6,11 @@ CREATE TABLE student(
     first_name VARCHAR(20),
     last_name VARCHAR(20)
 );
-
 CREATE TABLE book(
     book_id SERIAL PRIMARY KEY,
     book_name VARCHAR(255),
-    author VARCHAR(40),
-    borrow_by VARCHAR(40),
-    borrow_date DATE DEFAULT NULL,
-    return_date DATE DEFAULT NULL
+    author VARCHAR(100),
+    borrow_by VARCHAR(100),
+    borrow_date VARCHAR(100) DEFAULT NULL,
+    return_date VARCHAR(100) DEFAULT NULL
 );
