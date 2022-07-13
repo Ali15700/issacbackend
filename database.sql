1535016768e6f7eaaae5d6df5d7c9167d0390cf8
Ali <hubealii1570@gmail.com>
2022-07-13 06:15:53 +0500

    third commit

--- database.sql
+++ database.sql
@@ -3,15 +3,15 @@ CREATE DATABASE cqlibrary; -- Create a Table in DATABASE';
 -- Create a Student Table ';
 CREATE TABLE student(
     student_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20)
+    first_name VARCHAR(40),
+    last_name VARCHAR(40)
 );
 
 CREATE TABLE book(
     book_id SERIAL PRIMARY KEY,
     book_name VARCHAR(255),
-    author VARCHAR(40),
-    borrow_by VARCHAR(40),
-    borrow_date DATE DEFAULT NULL,
-    return_date DATE DEFAULT NULL
+    author VARCHAR(100),
+    borrow_by VARCHAR(100),
+    borrow_date VARCHAR(100) DEFAULT NULL,
+    return_date VARCHAR(100) DEFAULT NULL
 );
